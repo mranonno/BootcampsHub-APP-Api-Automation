17c7362db6dc6c093a2eb03a9ab0bedd7c7b6e26
Mitul das <mituldas751@gmail.com>
2024-11-23 16:26:44 +0600

    Auth

---
+++ cypress/e2e/1. Auth/1.4 verifyOtp.js
@@ -0,0 +1,41 @@
+import { faker } from "@faker-js/faker";
+
+describe("As a student I should be able to verify successfully with status code 200", () => {
+  let studentID;
+
+  before(() => {
+    cy.readFile("cypress/fixtures/rejisterId.json").then((data) => {
+      studentID = data.studentUserRegisterId;
+    });
+  });
+
+  it.skip("should be able to verify", () => {
+    const otp = "654911"; // Define OTP for verification
+
+    cy.request({
+      method: "POST",
+      url: "/api/user/verifyotp",
+      body: {
+        otp: otp,
+        channel: "email",
+        userId: studentID, // Use student ID obtained from fixture
+      },
+    }).then((response) => {
+      // Check if the request was successful
+      if (response.status === 200) {
+        cy.log("Verification successful"); // Log successful verification
+        console.log("Verification successful");
+        // Assertions
+        expect(response.status).to.eq(200); // Check if the status code is 200
+        expect(response.body).to.have.property("success", true); // Check if the success property is true
+        expect(response.duration).to.be.lessThan(2000);
+      } else {
+        // Handle unsuccessful verification
+        cy.log("Verification failed with status:", response.status);
+        cy.log(response.body);
+        console.log("Verification failed with status:", response.status);
+        console.log(response.body);
+      }
+    });
+  });
+});
---
+++ cypress/e2e/1. Auth/1.5 resetPassword.js
@@ -0,0 +1,53 @@
+describe("As a student, I should be able to reset my password successfully with status code 200", () => {
+    let studentEmail;
+    let studentNumber;
+    let accessToken;
+  
+    before(() => {
+      // Combine reading files using Cypress.Promise.all for efficiency
+      cy.wrap(
+        Cypress.Promise.all([
+          cy.readFile("cypress/fixtures/studentToken.json"),
+          cy.readFile("cypress/fixtures/userEmail.json"),
+          cy.readFile("cypress/fixtures/userNumber.json"),
+        ])
+      ).then(([tokenData, emailData, numberData]) => {
+        accessToken = tokenData.studentLoginToken;
+        studentEmail = emailData.studentUserEmail;
+        studentNumber = numberData.studentUserNumber;
+        // console.log(accessToken, studentEmail, studentNumber);
+      });
+    });
+  
+    it.skip("Checking if the user can reset their password or not", () => {
+      cy.request({
+        method: "PATCH",
+        url: "/api/user/password/reset",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: {
+          email: studentEmail,
+          phone: studentNumber,
+          channel: "email",
+          otp: "268660", // User valid OTP
+          password: "Ashraful186@",
+        },
+        failOnStatusCode: false, // Handle non-2xx status codes manually
+      }).then((response) => {
+        if (response.status === 200) {
+          // Assertions
+          expect(response.status).to.eq(200); // Check if the status code is 200
+          expect(response.body).to.have.property("success", true); // Check if the success property is true
+          expect(response.duration).to.be.lessThan(2000);
+          // Log the response for debugging
+          cy.log("Password Reset Response:", response.body);
+          console.log("Password Reset Response:", response.body);
+        } else {
+          cy.log("Reset password failed with status code: ", response.status);
+          cy.log(response.body.error);
+        }
+      });
+    });
+  });
+  
