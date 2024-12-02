9314ec4d6d731d538cc4fa4004f525a8bddcbb99
Anonno Das <anonnodas97@gmail.com>
2024-12-02 20:11:02 +0600

    Add and remove invitation user

--- cypress/e2e/8. Calendar Service/02. Invitation/8.02.1 Add User/8.02.1 addUser.cy.js
+++ cypress/e2e/8. Calendar Service/02. Invitation/8.02.1 Add User/8.02.1 addUser.cy.js
@@ -1,23 +1,23 @@
-describe("Get my show n tell successfully with status code 200", () => {
+describe("Add invitation user successfully with status code 200", () => {
   let accessToken;
-  let enrollment;
+  let eventId = "6737b7029c8d6a9880ee968a";
 
   before(() => {
     cy.readFile("cypress/fixtures/studentToken.json").then((tokenData) => {
       accessToken = tokenData.studentLoginToken;
     });
-    cy.readFile("cypress/fixtures/studentLoginID.json").then((loginData) => {
-      enrollment = loginData.enrollmentId;
-    });
   });
 
-  it("Checking if should be able Get my show n tell or not", () => {
+  it("Checking if should be able Add invitation user or not", () => {
     cy.request({
-      method: "GET",
-      url: "/show-tell/myshows",
+      method: "PATCH",
+      url: `/calendar/event/invitation/${eventId}`,
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        Enrollment: enrollment,
+      },
+      body: {
+        action: "add",
+        user: "650e70e7165fc20019352988",
       },
 
       failOnStatusCode: false,
@@ -29,10 +29,13 @@ describe("Get my show n tell successfully with status code 200", () => {
         expect(response.body).to.have.property("success", true);
         // Log the response for debugging
         cy.log("response.body", JSON.stringify(response.body, null, 1));
-        cy.log("Get my show n tell Response:", response.body);
-        console.log("Get my show n tell Response:", response.body);
+        cy.log("Add invitation user Response:", response.body);
+        console.log("Add invitation user Response:", response.body);
       } else {
-        cy.log("Get my show n tell failed with status code: ", response.status);
+        cy.log(
+          "Add invitation user failed with status code: ",
+          response.status
+        );
         cy.log(response.body.error);
       }
     });
