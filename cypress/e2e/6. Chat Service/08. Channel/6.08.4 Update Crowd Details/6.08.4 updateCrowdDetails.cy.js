6d011e5ff9612981bd508e09add589f1240f646d
Anonno Das <anonnodas97@gmail.com>
2024-12-03 19:30:52 +0600

    update crowd details and update crowd avatar

--- cypress/e2e/6. Chat Service/08. Channel/6.08.4 Update Crowd Details/6.08.4 updateCrowdDetails.cy.js
+++ cypress/e2e/6. Chat Service/08. Channel/6.08.4 Update Crowd Details/6.08.4 updateCrowdDetails.cy.js
@@ -1,42 +1,47 @@
-describe.skip("Add member to channel with status code 200", () => {
+describe("Update crowd details with status code 200", () => {
   let accessToken;
-  let chatId;
-  let memberId;
+  let organizationId;
+  let crowdId;
 
   before(() => {
     cy.readFile("cypress/fixtures/studentToken.json").then((tokenData) => {
       accessToken = tokenData.studentLoginToken;
     });
     cy.readFile("cypress/fixtures/studentLoginID.json").then((loginData) => {
-      chatId = loginData.chatId;
-      memberId = loginData.memberId;
+      organizationId = loginData.organizationId;
+    });
+    cy.readFile("cypress/fixtures/crowdId.json").then((crowdData) => {
+      crowdId = crowdData.crowdId;
     });
   });
 
-  it("Checking if should be able Add member to channel or not", () => {
+  it("Checking if should be able Update crowd details or not", () => {
     cy.request({
       method: "PATCH",
-      url: `/chat/channel/adduser/${chatId}`,
+      url: `/chat/channel/update/${crowdId}`,
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        // Organization: organizationId,
+        Organization: organizationId,
       },
       body: {
-        member: memberId,
+        name: "Channel no 2",
+        description: "Nothing",
+        isReadOnly: false,
+        isPublic: true,
       },
       failOnStatusCode: false,
     }).then((response) => {
       if (response.status === 200) {
         // Assertions
         expect(response.status).to.eq(200);
-        expect(response.duration).to.be.lessThan(2000);
+        expect(response.duration).to.be.lessThan(3000);
         expect(response.body).to.have.property("success", true);
         // Log the response for debugging
         cy.log("response.body", JSON.stringify(response.body, null, 1));
-        console.log("Add member to channel Response:", response.body);
+        console.log("Update crowd details Response:", response.body);
       } else {
         cy.log(
-          "Add member to channel failed with status code: ",
+          "Update crowd details failed with status code: ",
           response.status
         );
         cy.log(response.body.error);
