a14dd40fbdd9fe6850653c5c8a64fe5b6197e3d0
Anonno Das <anonnodas97@gmail.com>
2024-12-03 20:21:58 +0600

    mute,unmute,block,unblock

--- cypress/e2e/6. Chat Service/09. Member Action/6.09.4 Block Member/6.09.4 blockMember.cy.js
+++ cypress/e2e/6. Chat Service/09. Member Action/6.09.4 Block Member/6.09.4 blockMember.cy.js
@@ -1,38 +1,44 @@
-describe("Get my events successfully with status code 200", () => {
+describe("Block member from crowd with status code 200", () => {
   let accessToken;
-  let organizationId;
+  let chatId;
+  let member = "674e7e06f987f5001a56c5f8";
 
   before(() => {
     cy.readFile("cypress/fixtures/studentToken.json").then((tokenData) => {
       accessToken = tokenData.studentLoginToken;
     });
-    cy.readFile("cypress/fixtures/organizationID.json").then((orgData) => {
-      organizationId = orgData.organizationId;
+    cy.readFile("cypress/fixtures/studentLoginID.json").then((loginData) => {
+      chatId = loginData.chatId;
     });
   });
 
-  it("Checking if should be able Get my events or not", () => {
+  it("Checking if should be able Block member from crowd or not", () => {
     cy.request({
-      method: "GET",
-      url: "/calendar/event/myevents",
+      method: "POST",
+      url: "/chat/member/update",
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        Organization: organizationId,
       },
-
+      body: {
+        member: member,
+        chat: chatId,
+        actionType: "block",
+      },
       failOnStatusCode: false,
     }).then((response) => {
       if (response.status === 200) {
         // Assertions
         expect(response.status).to.eq(200);
-        expect(response.duration).to.be.lessThan(2000);
+        expect(response.duration).to.be.lessThan(3000);
         expect(response.body).to.have.property("success", true);
         // Log the response for debugging
         cy.log("response.body", JSON.stringify(response.body, null, 1));
-        cy.log("Get my events Response:", response.body);
-        console.log("Get my events Response:", response.body);
+        console.log("Block member from crowd Response:", response.body);
       } else {
-        cy.log("Get my events failed with status code: ", response.status);
+        cy.log(
+          "Block member from crowd failed with status code: ",
+          response.status
+        );
         cy.log(response.body.error);
       }
     });
